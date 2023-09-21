23d4ff4f5f28648ca9788c13de3ea5abde520150
bagus faize <bagusfaize@gmail.com>
2023-09-21 23:35:52 +0700

    add dummy food list

---
+++ app/food/page.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import { IoIosAdd } from 'react-icons/io';
+
+const foods = [
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+  {
+    title:"Karedok",
+    desc: "Raw vegetable salad in peanut sauce from West Java, Indonesia.",
+    imgSrc: "/images/food-1.png"
+  },
+]
+
+export default function Food() {
+    return (
+      <main className="p-5">
+        <h1 className="text-3xl font-semibold mb-5">All Foods</h1>
+        <div className="grid grid-cols-12 gap-x-3 gap-y-7">
+          {foods.map(item => (
+            <div className="bg-white p-5 rounded-2xl col-span-3 w-48 shadow-sm" key={item.title}>
+              <div><Image src={item.imgSrc} alt="food-img" width={100} height={100}/></div>
+              <div>
+                <div className="text-sm font-semibold">{item.title}</div>
+                <div className="text-sm text-gray-500">{item.desc}</div>
+              </div>
+              <div className="flex justify-between mt-2 items-end">
+                <span className="font-semibold">Rp 20k</span>
+                <span className="h-7 w-7 bg-black rounded-full flex justify-center items-center cursor-pointer">
+                  <IoIosAdd className="text-2xl text-white" />
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    )
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="p-5">
-      <h1 className="text-3xl font-bold">Craving Something Delicious?</h1>
+      <h1 className="text-3xl font-semibold">Craving Something Delicious?</h1>
       <h1 className="text-xl text-slate-600">Order Now</h1>
     </main>
   )
