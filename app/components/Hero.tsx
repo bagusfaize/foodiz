7a6334c1fcd619afc206b98492697f8d7920e257
bagus faize <bagusfaize@gmail.com>
2023-09-26 23:58:54 +0700

    add hero

---
+++ app/components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="bg-main-yellow rounded-xl p-5 h-56">
+        <h1 className="text-3xl text-white font-medium">Craving Something Delicious?</h1>
+        <h2 className="text-white font-medium text-lg">Order Now</h2>
+    </div>
+  )
+}
