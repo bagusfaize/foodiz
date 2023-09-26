bdfd9171ed27beef80da8381bd1f4271c6729916
bagus faize <bagusfaize@gmail.com>
2023-09-26 23:59:53 +0700

    delete unused component

--- app/components/BasketButton.tsx
+++
@@ -1,9 +0,0 @@
-import { BiBasket } from "react-icons/bi";
-
-export default function BasketButton(){
-    return(
-      <div className="bg-white w-16 h-16 flex justify-center items-center rounded-lg shadow-lg absolute bottom-10 right-10">
-        <BiBasket className="text-2xl"/>
-      </div>
-    )
-  }
