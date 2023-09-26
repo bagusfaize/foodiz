2e40f32e2321b6757149996f5370df56337a40a3
bagus faize <bagusfaize@gmail.com>
2023-09-26 23:59:35 +0700

    add constant

---
+++ app/constant/constant.ts
@@ -0,0 +1,57 @@
+export const foods = [
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+    {
+      title:"Karedok",
+      desc: "Raw vegetable salad in peanut sauce.",
+      imgSrc: "/images/food-1.png"
+    },
+  ]
